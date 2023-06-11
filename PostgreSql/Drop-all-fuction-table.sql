af06f080a2532794512bead5040f428201ee2de8
nguyetanhmyangel <nguyetanhmyangel@gmail.com>
2023-06-11 15:12:43 +0700

    2th commit

---
+++ PostgreSql/Drop-all-fuction-table.sql
@@ -0,0 +1,39 @@
+DO
+$do$
+DECLARE
+   _sql text;
+BEGIN
+   SELECT INTO _sql
+          string_agg(format('DROP %s %s;'
+                          , CASE prokind
+                              WHEN 'f' THEN 'FUNCTION'
+                              WHEN 'a' THEN 'AGGREGATE'
+                              WHEN 'p' THEN 'PROCEDURE'
+                              WHEN 'w' THEN 'FUNCTION'  -- window function (rarely applicable)
+                              -- ELSE NULL              -- not possible in pg 11
+                            END
+                          , oid::regprocedure)
+                   , E'\n')
+   FROM   pg_proc
+   WHERE  pronamespace = 'public'::regnamespace  -- schema name here!
+   -- AND    prokind = ANY ('{f,a,p,w}')         -- optionally filter kinds
+   ;
+
+   IF _sql IS NOT NULL THEN
+      RAISE NOTICE '%', _sql;  -- debug / check first
+      EXECUTE _sql;         -- uncomment payload once you are sure
+   ELSE
+      RAISE NOTICE 'No fuctions found in schema %', quote_ident(_schema);
+   END IF;
+END
+$do$;
+
+--- 
+
+DO $$ DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+    END LOOP;
+END $$;
